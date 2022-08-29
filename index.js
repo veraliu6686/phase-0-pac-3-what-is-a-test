19ae62613fb584e9252b47443765255dc02e4703
lianzi liu <lianziliu.sugar@gmail.com>
2022-08-29 13:10:35 -0400

    finished assignment

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-const name = "Joe";
-const height = 74;
-const message = `${name} has a ${height} inches snake`;
+const name = "Susan";
+const height = 35;
+const message = `${name} has a ${height} inches dog`;
 
 module.exports = {name, height, message};
-
-console.log(message);
