f5618ecd7cb1e7fb18d06ec7812299a718414941
veraliu6686 <109030778+veraliu6686@users.noreply.github.com>
2022-08-30 10:18:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const name = "Susan";
 const height = 35;
-const message = `${name} has a ${height} inches dog`;
+const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
